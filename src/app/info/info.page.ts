d1a55f840799d59e83cec34a01062b8a0cd97199
WilliamFischer <williamfischer363@gmail.com>
2019-07-07 16:32:22 +1000

    Fixed more home

--- src/app/info/info.page.ts
+++ src/app/info/info.page.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-info',
   templateUrl: './info.page.html',
   styleUrls: ['./info.page.scss'],
 })
 export class InfoPage implements OnInit {
+  emptyInput: boolean = true;
+  shopLocation: string;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+  }
+
+  getLocation($event){
+    var searchQuery = $event.srcElement.value;
+
+    if(searchQuery.length >= 1){
+      this.emptyInput = false;
+    }else{
+      this.emptyInput = true;
+    }
+  }
+
+  goHome(){
+    this.router.navigateByUrl('/home');
   }
 
 }
